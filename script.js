2e9d034e33a46cdb81c3ea378031e2f94cdc8907
xtrevad <trevor.adelson@gmail.com>
2022-02-16 21:37:58 +1100

    end of day feb 16 commit

--- script.js
+++ script.js
@@ -6,22 +6,71 @@ gameArray.length = 9;
 const gameBoard = (() => {
     let counter = 1;
     const changeTile = () => {
-        counter++;
         for (let i = 0; i < 9; i++) {
             const tile = document.getElementById(`tile-${i+1}`)
             if (event.target === tile) {
-                if (counter%2 === 0) {
-                    gameArray[i] = 'x';     
+                if (counter%2 === 0 && gameArray[i] === undefined) {
+                    gameArray[i] = 'x';
+                    counter++;     
                 };
-                if (counter%2 === 1) {
+                if (counter%2 === 1 && gameArray[i] === undefined) {
                     gameArray[i] = 'o';
-                }
+                    counter++;
+                };
             };
         };
         displayController.display();
+        checkResult();
+    };
+    const checkResult = () => {
+        if ((gameArray[0] === gameArray[1]) && // checks row 1
+            (gameArray[1] === gameArray[2]) &&
+            (gameArray[0] !== undefined) ||  
+            (gameArray[3] === gameArray[4]) && // checks row 2
+            (gameArray[4] === gameArray[5]) &&
+            (gameArray[3] !== undefined) ||
+            (gameArray[6] === gameArray[7]) && // checks row 3
+            (gameArray[7] === gameArray[8]) &&
+            (gameArray[6] !== undefined) ||
+            (gameArray[0] === gameArray[3]) && // checks column 1
+            (gameArray[3] === gameArray[6]) &&
+            (gameArray[0] !== undefined) ||
+            (gameArray[1] === gameArray[4]) && // checks column 2
+            (gameArray[4] === gameArray[7]) &&
+            (gameArray[1] !== undefined) ||
+            (gameArray[2] === gameArray[5]) && // checks column 3
+            (gameArray[5] === gameArray[8]) &&
+            (gameArray[2] !== undefined) ||
+            (gameArray[0] === gameArray[4]) && // diagonal top-L to btm-R
+            (gameArray[4] === gameArray[8]) &&
+            (gameArray[0] !== undefined) ||
+            (gameArray[2] === gameArray[4]) && // diagonal top-R to btm-L
+            (gameArray[4] === gameArray[6]) &&
+            (gameArray[2] !== undefined)
+            ) {
+                alert('winner, game over')
+            } else if (!isFull) { // THIS AIN'T WORKING
+                alert('nobody wins')
+            };
+        };
+    const isFull = () => {  // THIS AIN'T WORKING
+        for (let i = 0; i < 9; i++) {
+            if (gameArray[i] === undefined) {
+                continue;
+            };
+        }
+    }
+    const reset = () => {
+        for (let i = 0; i < 9; i++) {
+            gameArray[i] = undefined;
+            const tile = document.getElementById(`tile-${i+1}`);
+            tile.classList.remove('tile-cross');
+            tile.classList.remove('tile-naught');
+        };
     };
     return {
         changeTile,
+        reset,
     };
 })();
 
@@ -60,16 +109,8 @@ const displayController = (() => {
             };
         };
     };
-    const reset = () => {
-        for (let i = 0; i < 9; i++) {
-            const tile = document.getElementById(`tile-${i+1}`);
-            tile.classList.remove('tile-cross');
-            tile.classList.remove('tile-naught');
-        };
-    };
     return {
         display,
-        reset,
     }
 })();
 
